a060cbc6a5da11f969786a86e6cb1bb802745205
brayanvilla555 <brayanalexander098@gmail.com>
2024-02-24 20:24:28 -0500

    Configuración de la conexión

---
+++ Config/Conexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Programacion_aplicada_dos.Config
+{
+    internal class Conexion
+    {
+        private string servidor = ".";
+        private string bd = "negocios";
+        private string usuario = "";
+        private string passwor = "";
+        public SqlConnection sqlConnection = null;
+
+        public void conectar()
+        {
+            string connectionString = $"Data Source ={servidor}; Initial Catalog = {bd}; Integrated Security = True";
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                //abrimos la conexion
+                sqlConnection.Open();
+                //cerramos la condicion
+                //sqlConnection.Close();
+            }catch (Exception ex) { 
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        
+    }
+}
